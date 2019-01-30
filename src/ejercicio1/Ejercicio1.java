abbbfba10ef896fec1e1d49816d7d4ab03cd506f
Sergio Gutierrez <sergio.gutierrez@cesjuanpablosegundo.es>
2019-01-30 10:35:49 +0100

    Version inicial

---
+++ src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author Usuario DAM 1
+ */
+
+import java.util.Scanner;
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        
+        double primeranota;
+        double segundanota;
+        double mediadeseada;
+        
+        System.out.println("Introduce la nota del primer examen: ");
+        primeranota = sc.nextDouble();        
+        primeranota = primeranota *0.4;
+        
+        System.out.println("¿Qué nota quieres sacar en el trimestre: ");
+        mediadeseada= sc.nextDouble();
+        
+        
+        
+        
+    }
+    
+}
